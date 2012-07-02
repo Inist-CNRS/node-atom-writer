7af979fff516fb0c5d7ddf617c6d67ec4cb2ca3c
Nicolas Thouvenin <nthouvenin@gmail.com>
2012-07-02 17:01:44 +0200

    tests are ok

--- test/basic.js
+++ test/basic.js
@@ -3,14 +3,18 @@ var XMLWriter = require('xml-writer')
 var ATOMWriter = require('../')
 
 exports['setUp'] = function (callback) {
-  this.xw = new XMLWriter(true)
+  this.xw = new XMLWriter(false)
   this.aw = new ATOMWriter(this.xw);
   callback();
 }
 
 exports['t01'] = function (test) {
+
+  var created = new Date(365 * 24 * 60 * 60 * 1000)
+  var updated = new Date(365 * 24 * 60 * 60 * 1000)
+
   this.aw
-  .startFeed('urn:tps:com-idx')
+  .startFeed('urn:tps:com-idx', updated, created)
   .writeStartIndex(1)
   .writeItemsPerPage(10)
   .writeTotalResults(100)
@@ -18,7 +22,7 @@ exports['t01'] = function (test) {
   .writeLink('http://exemple.com/feed.xml', 'application/atom+xml', 'self')
 
   this.aw
-  .startEntry('urn:tps:com-idx-1')
+  .startEntry('urn:tps:com-idx-1', updated, created)
   .writeTitle('Data 1')
   .writeLink('/1.xml', 'text/xml')
   .writeLink('/1.txt', 'text/plain')
@@ -28,7 +32,7 @@ exports['t01'] = function (test) {
   .endEntry()
 
   this.aw
-  .startEntry('urn:tps:com-idx-2')
+  .startEntry('urn:tps:com-idx-2', updated, created)
   .writeTitle('Data 2')
   .writeLink('2.txt', 'text/plain')
   .writeContent('deux', 'text', 'fr')
@@ -37,8 +41,7 @@ exports['t01'] = function (test) {
 
   this.aw
   .endFeed()
-
-  console.log(this.xw.toString())
-
+  //console.log(this.xw.toString())
+  test.equal(this.xw.toString(), '<feed xmlns="http://www.w3.org/2005/Atom" xmlns:opensearch="http://a9.com/-/spec/opensearch/1.1/"><id>urn:tps:com-idx</id><created>1971-01-01T00:00:00Z</created><updated>1971-01-01T00:00:00Z</updated>1</feed><opensearch:itemsPerPage>10</opensearch:itemsPerPage><opensearch:totalResults>100</opensearch:totalResults><title type="text">Index of /</title><link type="application/atom+xml" href="http://exemple.com/feed.xml" rel="self"/><entry><id>urn:tps:com-idx-1</id><updated>1971-01-01T00:00:00Z</updated><published>1971-01-01T00:00:00Z</published><title type="text">Data 1</title><link type="text/xml" href="/1.xml"/><link type="text/plain" href="/1.txt"/><content type="text" xml:lang="fr">Un</content><author><name>Tata Toto</name><email>toto@exemple.com</email></author><category term="term" scheme="http://exemple.com#scheme"/></entry><entry><id>urn:tps:com-idx-2</id><updated>1971-01-01T00:00:00Z</updated><published>1971-01-01T00:00:00Z</published><title type="text">Data 2</title><link type="text/plain" href="2.txt"/><content type="text" xml:lang="fr">deux</content><author><name>titi.toto</name><email>titi.toto@exemple.com</email></author></entry>')
   test.done();
 };
